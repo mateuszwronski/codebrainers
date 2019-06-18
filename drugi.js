c855e60748684b0353e689596d9dfba2d0c00910
Mateusz Wroński <wronsky7@gmail.com>
2019-06-18 20:24:37 +0200

    edycja pierwszego.js i dodanie drugiego.js

---
+++ drugi.js
@@ -0,0 +1,15 @@
+<html>
+<head>
+My first JavaScript!
+</head>
+<body>
+<br>
+To jest normalny dokument HTML.
+<br>
+  <script language="LiveScript">
+    document.write("To jest JavaScript!")
+  </script>
+<br>
+I znowu dokument HTML.
+</body>
+</html>
